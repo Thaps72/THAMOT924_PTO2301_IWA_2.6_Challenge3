545b9dcdbb3a1c7ece28161943e90e5775691b9c
Thapelo A Motsumi <Thapelo.umuziphotoclub@gmail.com>
2023-04-19 23:59:04 +0200

    first commit

---
+++ script.js
@@ -0,0 +1,14 @@
+/*
+It is important to show the following message in oder to:
+-to asure users
+-to scare hackers
+-to impress investors
+*/
+
+const warn = 'Security scan starting';
+const info = 'please wait for scan to complete before closing the browser.';
+console.log("warn:", warn, info);
+
+/* it is important to let user know when they can close the page*/
+
+/*console.info( 'please wait for scan to complete before closing the browser.)*/
